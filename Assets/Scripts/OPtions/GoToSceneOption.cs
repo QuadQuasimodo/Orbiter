f6fb4d2eb3ab40fc34f07506fca9b5a4243857c2
RafaelCS-Aula <castroesilva.rafael@gmail.com>
2021-10-26 02:19:55 +0100

    Add naughty attributes package

--- Assets/Scripts/OPtions/GoToSceneOption.cs
+++ Assets/Scripts/OPtions/GoToSceneOption.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using NaughtyAttributes;
+
 
 public class ReturnToStartOption : Option
 {
+    [Scene]
+    public string targetScene;
     public override void OnActive()
     {
         SceneManager.LoadScene("StartScene");
